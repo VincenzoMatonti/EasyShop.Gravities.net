911341c6a6ffde82e3a2bd21b85496ddbe56739f
Vincenzo Matonti <tua-email@esempio.com>
2026-07-10 01:20:25 +0200

    create controller for dashboard and customerprofile

--- app/Http/Controllers/Customer/CustomerProfileController.php
+++ app/Http/Controllers/Customer/CustomerProfileController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Identity\User;
+use App\Services\Customer\CustomerContextService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CustomerProfileController extends Controller
 {
@@ -14,6 +17,28 @@ class CustomerProfileController extends Controller
 
     public function select()
     {
-        return view('customer.profile.select');
+        /** @var User $user */
+        $user = Auth::user();
+
+        $profiles = $user->getActiveCustomerProfiles();
+
+        return view('customer.profile.select', compact('profiles'));
+    }
+
+
+    public function switch(Request $request, CustomerContextService $context)
+    {
+        /** @var User $user */
+        $user = Auth::user();
+
+        $customerProfileId = $request->integer('customer_profile_id');
+
+        if (!$user->hasCustomerProfile($customerProfileId)) {
+            abort(403);
+        }
+
+        $context->setById($customerProfileId);
+
+        return redirect()->route('customer.dashboard');
     }
 }
