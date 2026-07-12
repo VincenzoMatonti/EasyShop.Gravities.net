c80567d8f374efac7145bbb37c1264ca2aa5da31
Vincenzo Matonti <tua-email@esempio.com>
2026-07-12 02:49:15 +0200

    add ui to dashbord customer

--- resources/views/customer/dashboard/index.blade.php
+++ resources/views/customer/dashboard/index.blade.php
@@ -3,6 +3,13 @@
     <div class="container-fluid mybg mt-5">
         <div class="row justify-content-center align-items-center">
             <div class="col-12 col-md-6 height-custom mt-3">
+                <div class="text-center">
+                    <h1 class="display-5"> Benvenuto nella tua area cliente </h1>
+                    <p class="lead">Gestisci la tua area e accedi ai tuoi servizi.</p>
+                    <a href="{{ route('customer.index') }}" class="btn btn-secondary w-100 mt-2">
+                        Go back
+                    </a>
+                </div>
             </div>
         </div>
     </div>
