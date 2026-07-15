08e57de27be38b74c18373393f006fc833e76531
Vincenzo Matonti <tua-email@esempio.com>
2026-07-15 23:47:24 +0200

    fix app name

--- resources/views/components/layouts/main-layout.blade.php
+++ resources/views/components/layouts/main-layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{env('APP_NAME')}} - {{$title}}</title>
+    <title>{{ config('app.name') }} - {{$title}}</title>
     <!-- CDN fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
     <!-- Bundling asset -->
