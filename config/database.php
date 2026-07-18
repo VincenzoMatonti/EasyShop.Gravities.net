487ed0ca20874891467a7f59f147c77af5951ae5
Vincenzo Matonti <tua-email@esempio.com>
2026-07-18 13:52:38 +0200

    Fix Pint formatting

--- config/database.php
+++ config/database.php
@@ -149,7 +149,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
             'scheme' => 'tls',
         ],
@@ -162,7 +162,7 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
             'scheme' => 'tls',
-            'ssl' => ['verify_peer' => false,],
+            'ssl' => ['verify_peer' => false],
             'max_retries' => env('REDIS_MAX_RETRIES', 3),
             'backoff_algorithm' => env('REDIS_BACKOFF_ALGORITHM', 'decorrelated_jitter'),
             'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
