036ce670e5f6597ffac764839ce6420d23a740b9
Vincenzo Matonti <tua-email@esempio.com>
2026-07-19 11:14:19 +0200

    fix config to autodeploy ton render

--- config/database.php
+++ config/database.php
@@ -149,7 +149,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
             'scheme' => env('REDIS_SCHEME'),
         ],
@@ -161,7 +161,7 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
-            'scheme' => env('REDIS_SCHEME'),
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
             'ssl' => env('REDIS_SCHEME') === 'tls' ? ['verify_peer' => false] : [],
             'max_retries' => env('REDIS_MAX_RETRIES', 3),
             'backoff_algorithm' => env('REDIS_BACKOFF_ALGORITHM', 'decorrelated_jitter'),
@@ -176,7 +176,8 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
-            'scheme' => env('REDIS_SCHEME'),
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
+            'ssl' => env('REDIS_SCHEME') === 'tls' ? ['verify_peer' => false] : [],
             'max_retries' => env('REDIS_MAX_RETRIES', 3),
             'backoff_algorithm' => env('REDIS_BACKOFF_ALGORITHM', 'decorrelated_jitter'),
             'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
