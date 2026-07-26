5131f5a919aa9d11d87faeb94889e6e7e7390de2
Vincenzo Matonti <tua-email@esempio.com>
2026-07-26 20:29:09 +0200

    fix config database for redis

--- config/database.php
+++ config/database.php
@@ -155,7 +155,7 @@ return [
         ],
 
         'default' => [
-            'url' => env('REDIS_URL'),
+            'url' => env('REDIS_URL') ?: null,
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
@@ -170,7 +170,7 @@ return [
         ],
 
         'cache' => [
-            'url' => env('REDIS_URL'),
+            'url' => env('REDIS_URL') ?: null,
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
